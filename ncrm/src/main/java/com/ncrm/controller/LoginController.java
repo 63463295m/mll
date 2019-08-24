0ecf9217340abe38459ab1c6766862b78581c24b
linlong <63463295@qq.com>
2019-08-25 00:35:49 +0900

    清空多饮用的依赖包

--- ncrm/src/main/java/com/ncrm/controller/LoginController.java
+++ ncrm/src/main/java/com/ncrm/controller/LoginController.java
@@ -1,15 +1,13 @@
 package com.ncrm.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ncrm.entity.Login;
 import com.ncrm.service.LoginService;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
